8cd5df5ea03c582d15ad0deca440178d96c13174
Joel Ndiba Mwaura <90656444+xjomwnd@users.noreply.github.com>
2023-07-04 03:02:15 +0300

    commit

--- css/src/index.js
+++
@@ -1,9 +0,0 @@
-import { getRandomRecipe } from '../src/randomRecipe.js';
-import { displaySavedRecipes } from '../src/savedRecipe.js';
-
-// Call the getRandomRecipe function and display the recipe on the page
-const apiKey = '80cf1614d6df4af9a16d5be110db7167';
-getRandomRecipe(apiKey);
-
-// Call the displaySavedRecipes function to display saved recipes on the page
-displaySavedRecipes();
